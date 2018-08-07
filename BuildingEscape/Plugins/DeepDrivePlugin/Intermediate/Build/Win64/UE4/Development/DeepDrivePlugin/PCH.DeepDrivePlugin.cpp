adeca9a1b712c96f07462c41fbd157930a567b11
Jefferson Javier Hernández Panqueba <jeffersonh44@gmail.com>
2018-08-06 21:09:56 -0500

    remove Intermediate and Binaries plugin folder
    Former-commit-id: 37f3d00c34cd0b403fefba53a24408d67f41f1f2

--- BuildingEscape/Plugins/DeepDrivePlugin/Intermediate/Build/Win64/UE4/Development/DeepDrivePlugin/PCH.DeepDrivePlugin.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Jefferson/Documents/UnrealProjects/Section_03/BuildingEscape/Plugins/DeepDrivePlugin/Intermediate/Build/Win64/UE4/Development/DeepDrivePlugin/PCH.DeepDrivePlugin.h"
